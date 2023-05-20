45ccaf2ecb129b0393332d954eedb0b2794dad8d
uni-select <wydxjcrf157@126.com>
2023-05-20 17:55:34 +0800

    修改dev分支代码

--- demo.js
+++ demo.js
@@ -2,6 +2,7 @@
 window.onload = function(){
     let box = document.getElementsByClassName('box')[0];
     box.addEventListener('click',()=>{
-        alert('这是测试代码')    
+        alert('这是测试代码') 
+        console.log('这是dev分支代码')   
     })
 }

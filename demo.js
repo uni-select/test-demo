29ab397207d0fa8721086ab718adb90f68d4e051
uni-select <wydxjcrf157@126.com>
2023-05-25 16:26:52 +0800

    修改master分支代码

--- demo.js
+++ demo.js
@@ -3,6 +3,6 @@ window.onload = function(){
     let box = document.getElementsByClassName('box')[0];
     box.addEventListener('click',()=>{
         alert('这是测试代码') 
-        console.log('这是dev分支代码')   
+        console.log('这是master分支代码')   
     })
 }
